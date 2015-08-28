cd39a1548134174135642024a38b520f0f4cce04
andrewstellman <astellman@stellman-greene.com>
2015-08-27 19:55:43 -0500

    Updated app to read from a folder and write to a file

--- src/main/scala/com/stellmangreene/pbprdf/PbpRdfApp.scala
+++ src/main/scala/com/stellmangreene/pbprdf/PbpRdfApp.scala
@@ -17,32 +17,72 @@ import java.io.File
 import org.xml.sax.InputSource
 import com.stellmangreene.pbprdf.model._
 import com.stellmangreene.pbprdf.util.XmlHelper
+import scala.util.Try
+import scala.util.Success
+import java.io.FileOutputStream
 
 object PbpRdfApp extends App with LazyLogging {
 
-  logger.info("Reading HTML play by play")
-  
-  val INPUT = "src/test/resources/com/stellmangreene/pbprdf/test/htmldata/400610636.html"
+  def printUsageAndExit(message: Option[String] = None) = {
+    if (message.isDefined)
+      println(message.get)
+    println("usage: pbprdf folder filename.ttl")
+    println("  Read all of the files in the folder and attempt to process them")
+    println("  Write all plays for each game to filename.ttl")
+    System.exit(1)
+  }
 
-  val xmlStream = new FileInputStream(INPUT)
-  val rootElem = XmlHelper.parseXml(xmlStream)
-  val playByPlay = new EspnPlayByPlay("400610636", rootElem)
+  if (args.size != 2) {
+    printUsageAndExit()
+  } else {
 
-  var rep = new SailRepository(new MemoryStore)
-  rep.initialize
+    var folder: File = null
+    val inputFolderPath = args(0)
+    Try(new File(inputFolderPath)) match {
+      case Success(f) => { folder = f }
+      case _          => printUsageAndExit(Some(s"Unable to open folder: ${inputFolderPath}"))
+    }
 
-  logger.info("Generating triples from unit test play by play")
-  playByPlay.addRdf(rep)
+    if (!folder.exists || !folder.isDirectory)
+      printUsageAndExit(Some("Invalid folder: ${inputFolderPath}"))
 
-  logger.info("Writing Turtle")
-  
-  val conn = rep.getConnection
-  var statements = conn.getStatements(null, null, null, true)
+    val files = folder.listFiles
+    if (files.isEmpty)
+      printUsageAndExit(Some("No files found in folder: ${inputFolderPath}"))
 
-  var model = Iterations.addAll(statements, new LinkedHashModel)
-  model.setNamespace("rdf", RDF.NAMESPACE)
-  model.setNamespace("rdfs", RDFS.NAMESPACE)
-  model.setNamespace("xsd", XMLSchema.NAMESPACE)
-  model.setNamespace("pbprdf", Ontology.NAMESPACE)
-  Rio.write(model, System.out, RDFFormat.TURTLE)
+    val outputFilename = args(1)
+    if (new File(outputFilename).exists)
+      printUsageAndExit(Some(s"File already exists, will not overwrite: ${outputFilename}"))
+
+    logger.info(s"Reading ${files.size} files from folder ${inputFolderPath}")
+
+    var rep = new SailRepository(new MemoryStore)
+    rep.initialize
+
+    var i = 0
+    files.foreach(file => {
+      i += 1
+      logger.info(s"Reading plays from ${file.getName} (file ${i} of ${files.size})")
+      val xmlStream = new FileInputStream(file)
+      val rootElem = XmlHelper.parseXml(xmlStream)
+      val playByPlay = new EspnPlayByPlay(file.getName, rootElem)
+      playByPlay.addRdf(rep)
+    })
+
+    logger.info(s"Writing Turtle to ${outputFilename}")
+
+    val conn = rep.getConnection
+    var statements = conn.getStatements(null, null, null, true)
+
+    var model = Iterations.addAll(statements, new LinkedHashModel)
+    model.setNamespace("rdf", RDF.NAMESPACE)
+    model.setNamespace("rdfs", RDFS.NAMESPACE)
+    model.setNamespace("xsd", XMLSchema.NAMESPACE)
+    model.setNamespace("pbprdf", Ontology.NAMESPACE)
+    
+    val outputStream = new FileOutputStream(outputFilename)
+    Rio.write(model, outputStream, RDFFormat.TURTLE)
+    
+    logger.info("Finished writing")
+  }
 }

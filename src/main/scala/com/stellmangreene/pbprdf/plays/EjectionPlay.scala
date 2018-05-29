845928fae22035733b256aeb0444511548048787
Andrew Stellman <astellman@stellman-greene.com>
2018-05-29 17:01:24 -0500

    Added ejection plays

---
+++ src/main/scala/com/stellmangreene/pbprdf/plays/EjectionPlay.scala
@@ -0,0 +1,68 @@
+package com.stellmangreene.pbprdf.plays
+
+import org.openrdf.model.Resource
+import org.openrdf.model.URI
+import org.openrdf.model.Value
+import org.openrdf.model.vocabulary.RDF
+import org.openrdf.repository.Repository
+
+import com.stellmangreene.pbprdf.model.EntityUriFactory
+import com.stellmangreene.pbprdf.model.Ontology
+import com.typesafe.scalalogging.LazyLogging
+import com.stellmangreene.pbprdf.GamePeriodInfo
+
+import com.stellmangreene.pbprdf.util.RdfOperations._
+
+/**
+ * A play that represents an ejection
+ * <p>
+ * Examples:
+ * Aerial Powers ejected
+ *
+ * @param gameID
+ *        Unique ID of the game
+ * @param eventNumber
+ *        Sequential number of this event
+ * @param period
+ *        Period this occurred in (overtime starts with period 5)
+ * @param team
+ *        Name of the team
+ * @param play
+ *        Description of the play (eg. "Alyssa Thomas makes free throw 2 of 2")
+ * @param score
+ *        Game score ("10-4") - CURRENTLY IGNORED
+ *
+ * @author andrewstellman
+ */
+class EjectionPlay(gameUri: URI, eventNumber: Int, period: Int, time: String, team: String, play: String, score: String, gamePeriodInfo: GamePeriodInfo)
+  extends Play(gameUri: URI, eventNumber: Int, period: Int, time: String, team: String, play: String, score: String, gamePeriodInfo: GamePeriodInfo)
+  with LazyLogging {
+
+  override def addRdf(rep: Repository) = {
+    val triples: Set[(Resource, URI, Value)] =
+      play match {
+        case EjectionPlay.playByPlayRegex(playerEjected) => {
+          Set(
+              (eventUri, RDF.TYPE, Ontology.EJECTION),
+              (eventUri, Ontology.PLAYER_EJECTED, EntityUriFactory.getPlayerUri(playerEjected)))
+        }
+
+        case _ => { logger.warn(s"Unrecognized ejection play: ${play}"); Set() }
+      }
+
+    if (!triples.isEmpty)
+      rep.addTriples(triples)
+
+    super.addRdf(rep)
+  }
+
+}
+
+/**
+ * Companion object that defines a regex that can be used to check this play against a description
+ */
+object EjectionPlay extends PlayMatcher {
+
+  val playByPlayRegex = """^(.+) ejected$""".r
+
+}
---
+++ src/test/scala/com/stellmangreene/pbprdf/plays/test/EjectionPlaySpec.scala
@@ -0,0 +1,49 @@
+package com.stellmangreene.pbprdf.plays.test
+
+import org.openrdf.repository.sail.SailRepository
+import org.openrdf.sail.memory.MemoryStore
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+import com.stellmangreene.pbprdf.GamePeriodInfo
+import com.stellmangreene.pbprdf.plays.EjectionPlay
+import com.stellmangreene.pbprdf.test.TestUri
+
+import com.stellmangreene.pbprdf.util.RdfOperations._
+
+/**
+ * Test the EjectionPlay class
+ *
+ * @author andrewstellman
+ */
+class EjectionPlaySpec extends FlatSpec with Matchers {
+
+  behavior of "EjectionPlay"
+
+  // As long as each event has unique game and event IDs, they can all go into the same repository
+  val rep = new SailRepository(new MemoryStore)
+  rep.initialize
+
+  it should "parse a five-second violation" in {
+
+    val testUri = TestUri.create("400981090")
+    new EjectionPlay(testUri, 367, 4, "1:10", "Wings", "Aerial Powers ejected", "71-76", GamePeriodInfo.WNBAPeriodInfo).addRdf(rep)
+
+    rep.executeQuery("SELECT * { <http://stellman-greene.com/pbprdf/400981090/367> ?p ?o }")
+      .map(statement => (s"${statement.getValue("p").stringValue} -> ${statement.getValue("o").stringValue}"))
+      .toSet should be(
+        Set(
+          "http://www.w3.org/1999/02/22-rdf-syntax-ns#type -> http://stellman-greene.com/pbprdf#Event",
+          "http://www.w3.org/1999/02/22-rdf-syntax-ns#type -> http://stellman-greene.com/pbprdf#Play",
+          "http://www.w3.org/1999/02/22-rdf-syntax-ns#type -> http://stellman-greene.com/pbprdf#Ejection",
+          s"http://stellman-greene.com/pbprdf#inGame -> ${testUri.stringValue}",
+          "http://stellman-greene.com/pbprdf#period -> 4",
+          "http://stellman-greene.com/pbprdf#time -> 1:10",
+          "http://stellman-greene.com/pbprdf#secondsIntoGame -> 2330",
+          "http://stellman-greene.com/pbprdf#secondsLeftInPeriod -> 70",
+          "http://stellman-greene.com/pbprdf#forTeam -> http://stellman-greene.com/pbprdf/teams/Wings",
+          "http://stellman-greene.com/pbprdf#playerEjected -> http://stellman-greene.com/pbprdf/players/Aerial_Powers",
+          "http://www.w3.org/2000/01/rdf-schema#label -> Wings: Aerial Powers ejected"))
+  }
+
+}
